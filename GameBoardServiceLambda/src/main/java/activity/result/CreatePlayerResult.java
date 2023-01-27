62e8fc28488de45c747b8e933ac2e19c4ccdd25e
Matthew OBar <matthewobar@gmail.com>
2023-01-27 15:49:36 -0600

    saving for now

--- GameBoardServiceLambda/src/main/java/activity/result/CreatePlayerResult.java
+++ GameBoardServiceLambda/src/main/java/activity/result/CreatePlayerResult.java
@@ -1,4 +1,38 @@
 package activity.result;
 
+import models.PlayerModel;
+
 public class CreatePlayerResult {
+    private final PlayerModel player;
+
+    private CreatePlayerResult(PlayerModel player) {
+        this.player = player;
+    }
+
+    public PlayerModel getPlayer() {
+        return player;
+    }
+
+    @Override
+    public String toString() {
+        return "CreatePlayerResult{" +
+                "player=" + player +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+    public static class Builder {
+        private PlayerModel playerModel;
+
+        public Builder withPlayer(PlayerModel playerModel) {
+            this.playerModel = playerModel;
+            return this;
+        }
+        public CreatePlayerResult build() {
+            return new CreatePlayerResult(playerModel);
+        }
+    }
 }
