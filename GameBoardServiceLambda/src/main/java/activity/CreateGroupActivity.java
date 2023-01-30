1cfd64b6dd3dea75fa30236c780edf634a623fc5
Matthew OBar <matthewobar@gmail.com>
2023-01-30 14:47:57 -0600

    good curl response and tests passing

---
+++ GameBoardServiceLambda/src/main/java/activity/CreateGroupActivity.java
@@ -0,0 +1,62 @@
+package activity;
+
+import activity.request.CreateGroupRequest;
+import activity.result.CreateGroupResult;
+import converters.ModelConverter;
+import dynamodb.GroupDao;
+import dynamodb.models.Group;
+import models.GroupModel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Set;
+import java.util.UUID;
+import javax.inject.Inject;
+
+/**
+ * This API allows a user to create a new Group object.
+ */
+public class CreateGroupActivity {
+    private final Logger log = LogManager.getLogger();
+    private final GroupDao groupDao;
+
+    /**
+     * Instantiates a CreateGroupActivity object.
+     * @param groupDao GroupDao to access the Groups table.
+     */
+    @Inject
+    public CreateGroupActivity(GroupDao groupDao) {
+        this.groupDao = groupDao;
+    }
+
+    /**
+     * This method handles the incoming request by building a new Group object.
+     * @param createGroupRequest the request object containing the new Group object.
+     * @return CreateGroupResult object containing the API defined GroupModel.
+     */
+    public CreateGroupResult handleRequest(final CreateGroupRequest createGroupRequest) {
+        log.info("Received CreateGroupRequest {}", createGroupRequest);
+        String groupId = UUID.randomUUID().toString().substring(0, 5);
+        String groupName = createGroupRequest.getGroupName();
+        String favoriteGameId = createGroupRequest.getFavoriteGameId();
+        Set<String> gameIds = createGroupRequest.getGameIds();
+        Set<String> gameOutcomeIds = createGroupRequest.getGameOutcomeIds();
+        Set<String> playerIds = createGroupRequest.getPlayerIds();
+
+        Group group = new Group();
+        group.setGroupId(groupId);
+        group.setGroupName(groupName);
+        group.setFavoriteGameId(favoriteGameId);
+        group.setGameIds(gameIds);
+        group.setGameOutcomeIds(gameOutcomeIds);
+        group.setPlayerIds(playerIds);
+
+        groupDao.saveGroup(group);
+        GroupModel groupModel = new ModelConverter().toGroupModel(group);
+
+        return CreateGroupResult.builder()
+                .withGroupModel(groupModel)
+                .build();
+    }
+
+}
---
+++ GameBoardServiceLambda/src/test/java/activity/CreateGroupActivityTest.java
@@ -0,0 +1,66 @@
+package activity;
+
+import activity.request.CreateGroupRequest;
+import activity.result.CreateGroupResult;
+import dynamodb.GroupDao;
+import dynamodb.models.Group;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+public class CreateGroupActivityTest {
+
+    @Mock
+    private GroupDao groupDao;
+
+    private CreateGroupActivity createGroupActivity;
+
+    @BeforeEach
+    public void setUp() {
+        openMocks(this);
+        createGroupActivity = new CreateGroupActivity(groupDao);
+    }
+
+    @Test
+    public void handleRequest_CreatesAndSavesNewGroup_ReturnsCreatedGroup() {
+        //GIVEN
+        String groupName = "testname";
+        String favoriteGameId = "GME001";
+        Set<String> gameIds = Set.of("GME001", "GME002", "GME003");
+        Set<String> gameOutcomeIds = Set.of("GO001", "GO002", "GO003");
+        Set<String> playerIds = Set.of("P001", "P002", "P003");
+
+
+        CreateGroupRequest request = CreateGroupRequest.builder()
+                .withGroupName(groupName)
+                .withFavoriteGameId(favoriteGameId)
+                .withGameIds(gameIds)
+                .withGameOutcomeIds(gameOutcomeIds)
+                .withPlayerIds(playerIds)
+                .build();
+
+        //WHEN
+        CreateGroupResult result = createGroupActivity.handleRequest(request);
+
+        //THEN
+        verify(groupDao).saveGroup(any(Group.class));
+
+        assertNotNull(result.getGroupModel().getGroupId());
+        assertEquals(groupName, result.getGroupModel().getGroupName());
+        assertEquals(favoriteGameId, result.getGroupModel().getFavoriteGameId());
+        assertEquals(gameIds, result.getGroupModel().getGameIds());
+        assertEquals(gameOutcomeIds, result.getGroupModel().getGameOutcomeIds());
+        assertEquals(playerIds, result.getGroupModel().getPlayerIds());
+
+    }
+}
