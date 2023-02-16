01e968e3478d106446deec9457832c5ee482ada2
Matthew OBar <matthewobar@gmail.com>
2023-02-16 13:42:33 -0600

    curl works for updateGroup, needs tests and comments

--- GameBoardServiceLambda/src/main/java/lambda/UpdateGroupLambda.java
+++ GameBoardServiceLambda/src/main/java/lambda/UpdateGroupLambda.java
@@ -7,9 +7,23 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class UpdateGroupLambda
         extends LambdaActivityRunner<UpdateGroupRequest, UpdateGroupResult>
-        implements RequestHandler<AuthenticatedLambdaRequest<UpdateGroupRequest>, LambdaResponse> {
+        implements RequestHandler<LambdaRequest<UpdateGroupRequest>, LambdaResponse> {
     @Override
-    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<UpdateGroupRequest> input, Context context) {
-        return null;
+    public LambdaResponse handleRequest(LambdaRequest<UpdateGroupRequest> input, Context context) {
+        return super.runActivity(
+                () -> {
+                    UpdateGroupRequest request = input.fromBody(UpdateGroupRequest.class);
+                    return input.fromPath(path ->
+                            UpdateGroupRequest.builder()
+                                    .withGroupId(path.get("groupId"))
+                                    .withGroupName(request.getGroupName())
+                                    .withFavoriteGameId(request.getFavoriteGameId())
+                                    .withGameIds(request.getGameIds())
+                                    .withPlayerIds(request.getPlayerIds())
+                                    .withGameOutcomeIds(request.getGameOutcomeIds())
+                                    .build());
+                },
+                (request, serviceComponent) -> serviceComponent.provideUpdateGroupAcitivty().handleRequest(request)
+        );
     }
 }
